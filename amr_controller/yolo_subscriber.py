891e18d4c38daa2d2661baf316f96dd7a462d048
kimandreas <kimandreas@hotmail.com>
2024-11-22 18:25:46 +0900

    up day 4

---
+++ amr_controller/yolo_subscriber.py
@@ -0,0 +1,32 @@
+import rclpy
+from rclpy.node import Node
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge
+import cv2
+
+class ImageSubscriber(Node):
+    def __init__(self):
+        super().__init__('image_subscriber')
+        self.subscription = self.create_subscription(
+            Image,
+            'tracked_image',
+            self.listener_callback,
+            10)
+        self.bridge = CvBridge()
+
+    def listener_callback(self, msg):
+        frame = self.bridge.imgmsg_to_cv2(msg, desired_encoding='bgr8')
+        cv2.imshow('Tracked Image', frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            rclpy.shutdown()
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = ImageSubscriber()
+    rclpy.spin(node)
+    node.destroy_node()
+    cv2.destroyAllWindows()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
